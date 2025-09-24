7aba278913ed7029d0c5269788d18858d5577aef
Vishal <Vishalsharma805966@gmail.com>
2025-09-24 06:04:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144433
+let accountEmail="Vishal@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+// accountId=2 //NOT ALLOWED
+accountEmail="hdhd@gmail.com"
+accountPassword="2324242424"
+accountCity="Bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
