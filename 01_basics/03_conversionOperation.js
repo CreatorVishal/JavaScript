17ab071a2b8089764c0e2f20165b6ccc52b145e9
Vishal <Vishalsharma805966@gmail.com>
2025-09-25 06:01:42 +0000

    03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33"
 
 console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber= Number(score)
 console.log(typeof valueInNumber);
@@ -21,4 +21,47 @@ let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//*****************************Operations*************/
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Vishal"
+
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");//Don't use 
+
+// let num1, num2 , num3
+// num1= num2 = num3=2+2
+
+let gameCounter=100
+gameCounter++; //101
+++gameCounter;//102
+console.log(gameCounter);
+
+
+
+
+
+
 
